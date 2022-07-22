df91a18e9c1dd88b59fe0e3afee1892d6a1c5196
CarlosPalen <109358744+CarlosPalen@users.noreply.github.com>
2022-07-21 23:56:09 -0700

    second

--- src/index.js
+++ src/index.js
@@ -1,42 +1,18 @@
-import { useState } from "react";
-import ReactDOM from "react-dom/client";
+import {usetState} from "react";
+import { ReactDOM } from "react";
 
-function MyForm() {
-  const [inputs, setInputs] = useState({});
-
+function myForm() {
+  const [textarea,setTextarea] = usetState("The content of a textarea goes in the valeu attribute.");
   const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
+    setTextarea(event.target.value);
   }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(inputs);
-  }
-
+  
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Enter your name:
-      <input 
-        type="text" 
-        name="username" 
-        value={inputs.username || ""} 
-        onChange={handleChange}
-      />
-      </label>
-      <label>Enter your age:
-        <input 
-          type="number" 
-          name="age" 
-          value={inputs.age || ""} 
-          onChange={handleChange}
-        />
-        </label>
-        <input type="submit" />
+    <form>
+      <textarea value = {textarea} onChange = {handleChange}/>
     </form>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
+const root = ReactDOM.crateRoot(document.getElementById("root"));
+root.render(<myForm/>);
