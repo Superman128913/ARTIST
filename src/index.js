5359f851717924e53a2bc00fee261fbead57d652
CarlosPalen <109358744+CarlosPalen@users.noreply.github.com>
2022-07-22 00:58:51 -0700

    second

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import { useState } from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-function MyForm() {
-  const [textarea, setTextarea] = useState(
-    "This world who more than love sun."
-  );
-
-  const handleChange = (event) => {
-    setTextarea(event.target.value)
-  }
-
+const Header = () => {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans-Serif"
+  };
   return (
-    <form>
-      <textarea value={textarea} onChange={handleChange} />
-    </form>
-  )
+    <>
+      <h1 style={myStyle}>Hello Style!</h1>
+      <p>Add a little style!</p>
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
+root.render(<Header />);
